ffd97488fe315f38a6af640e52f2e88cd3eebe6c
sebahtes.om <sebahtes.om@gmail.com>
2024-02-22 15:18:04 +0300

    cha

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,15 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
 
 const app = express();
-const prismaClient = new PrismaClient();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/api/recipes/search", async (req, res) => {});
-
-app.get("/api/recipes/:recipeId/summary", async (req, res) => {});
-
-app.post("/api/recipes/favourite", async (req, res) => {});
-
-app.get("/api/recipes/favourite", async (req, res) => {});
-
-app.delete("/api/recipes/favourite", async (req, res) => {});
+app.get("/api/recipe/search", async (req, res) => {
+  res.json({ message: "success" });
+});
 
 app.listen(5000, () => {
-  console.log("server running on localhost:5000");
+  console.log("Server running on localhost:5000");
 });
